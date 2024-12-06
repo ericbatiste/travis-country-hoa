12f0dea88229a4c8e8989b2f18bb681db2e4f25f
Eric Batiste <ericbatiste4@gmail.com>
2024-12-06 13:13:29 -0700

    Fix spacing on Monthly Feature pg.

--- src/components/MonthlyFeatureContent.tsx
+++ src/components/MonthlyFeatureContent.tsx
@@ -11,8 +11,8 @@ export default async function MonthlyFeatureContent() {
       <h2 className="my-6 md:my-10 text-2xl md:text-5xl font-semibold text-blue text-center">
         Monthly Featured TCCSA Bylaw
       </h2>
-      <section className="px-6 md:px-20">
-        <article className="bg-beige shadow-2xl max-w-4xl p-8 md:p-14">
+      <section className="px-2 md:px-20">
+        <article className="bg-beige shadow-2xl max-w-4xl p-4 md:p-14">
           {content?.bylaw_text && (
             <div
               className="prose font-serif lext-lg md:text-xl"
